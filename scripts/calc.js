5a392d24fcb3bdbc2727fa24a2c176ab4c78f43e
SOliv1 <ubuntu@ip-172-31-37-245.ec2.internal>
2019-08-17 13:29:30 +0000

    update calcjs

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,13 @@
-function addition(numOne, numTwo) {
-     if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
-    return numOne + numTwo;
-    } else {
-        alert("Error!");
-    }
+Calculator = function() {
+    this.value = 0;
 }
 
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+       alert("Argument must be a number");
+
+    }
+
+};
