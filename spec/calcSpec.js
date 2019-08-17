7826a910b1191b134230bdad1c579ec5c602e8a5
Ubuntu <ubuntu@ip-172-31-37-245.ec2.internal>
2019-08-17 05:38:45 +0000

    defensive programming

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -6,7 +6,11 @@ describe("Calculator", function() {
         it("should not return 4 if the arguments given don't produce a sum of 4", function() {
            expect(addition(7, 19)).toBe(26);
         });
+        it("should return an error if we don't supply two numbers", function()  {
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        });
     });
-});;
-        
+ });
+       
+ 
         
